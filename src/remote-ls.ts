2ff3ed1b2cb8021537527be7af15fa1711c9c5fb
IronBlood <aplshenyang@gmail.com>
2025-12-03 15:02:30 +0800

    fix: if package use "npm:" as prefix

--- src/remote-ls.ts
+++ src/remote-ls.ts
@@ -164,48 +164,39 @@ export class RemoteLS {
 
     const nextTasks: QueueNode[] = [];
 
-    // dependencies is always included
-    for (const [name, range] of Object.entries(entry.dependencies ?? {})) {
-      nextTasks.push({
-        name,
-        version: range,
-        type: DependantType.default,
-        parent: fullName,
-      });
-    }
+    const enqueue_deps = (deps: Record<string, string> | undefined, type: DependantType) => {
+      for (const [name, spec] of Object.entries(deps ?? {})) {
+        const parsed = npa(spec.startsWith("npm:") ? spec :`${name}@${spec}`);
+        const target = parsed.type === "alias" ? (parsed as npa.AliasResult).subSpec : (parsed as npa.Result);
+        if (!["tag", "range", "version"].includes(target.type)) {
+          if (this.verbose) {
+            console.log(`skipping non-registry spec ${spec} for ${name} from ${fullName}`);
+          }
+          continue;
+        }
 
-    // optional is included as well, but won't be visited
-    for (const [name, range] of Object.entries(entry.optionalDependencies ?? {})) {
-      nextTasks.push({
-        name,
-        version: range,
-        type: DependantType.optional,
-        parent: fullName
-      });
-    }
-
-    if (this.development && !task.parent) {
-      for (const [name, range] of Object.entries(entry.devDependencies ?? {})) {
         nextTasks.push({
-          name,
-          version: range,
-          // only dev dependencies of the root package will be included as default
-          type: DependantType.default,
-          parent: fullName
+          name: target.name,
+          version: target.rawSpec ?? spec,
+          type,
+          parent: fullName,
         });
       }
+    };
+
+    // dependencies is always included
+    enqueue_deps(entry.dependencies, DependantType.default);
+
+    // optional is included as well, but won't be visited
+    enqueue_deps(entry.optionalDependencies, DependantType.optional);
+
+    if (this.development && !task.parent) {
+      enqueue_deps(entry.devDependencies, DependantType.default);
     }
 
     // starting from npm v7, peer dependencies are always included, recursively
     if (this.peer) {
-      for (const [name, range] of Object.entries(entry.peerDependencies ?? {})) {
-        nextTasks.push({
-          name,
-          version: range,
-          type: DependantType.default,
-          parent: fullName
-        });
-      }
+      enqueue_deps(entry.peerDependencies, DependantType.default);
     }
 
     if (this.verbose) {
